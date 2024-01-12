f8470ec5a1ac2611294e07dae188e01733905113
Franco <franco2005ponce@gmail.com>
2024-01-11 22:13:25 -0300

    primer commit

---
+++ e-commerce/src/components/CartWidget.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CartWidget = () => {
+  return (
+    <div>
+      <img src="../images/cart.png" alt="cart"/>
+      <p>4</p>
+    </div>
+  )
+}
+
+export default CartWidget
